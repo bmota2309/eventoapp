f72d25121ec1c1261926ce2b561318a5eb6bff59
Bruno Mota <bruno.mota@itspossiblefor.me>
2021-07-21 19:50:37 -0300

    Commit aplicacao funcionando Initial commit 21/07/21

---
+++ src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
@@ -0,0 +1,23 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@SpringBootApplication
+@ComponentScan({"com.eventoapp.controllers"}) 
+@EntityScan(basePackages= {"com.eventoapp.models"})
+@ComponentScan({"com.eventoapp"})
+@EnableJpaRepositories("com.eventoapp.repository")
+public class EventoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoappApplication.class, args);
+		System.out.print(new BCryptPasswordEncoder().encode("123"));
+	}
+
+}
---
+++ src/main/java/com/eventoapp/security/ImplementsUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.eventoapp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+
+import com.eventoapp.models.Usuario;
+import com.eventoapp.repository.UsuarioRepository;
+
+@Repository
+public class ImplementsUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository ur;
+	
+	@Override
+	public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+		Usuario usuario = ur.findByLogin(login);
+		
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Usuário não encontrado!");
+		}
+		return usuario;
+	}
+
+	
+	
+}
